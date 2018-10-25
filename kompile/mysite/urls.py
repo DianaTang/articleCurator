0e8e3b1a15b9514610439a341d42aafd963be157
DianaTang <dianayentang@gmail.com>
2018-10-25 00:37:57 -0400

    add templates

--- kompile/mysite/urls.py
+++ kompile/mysite/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('', views.index, name='index'),]
+app_name='mysite'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:category_id>/results', views.category_filter, name='results'),
+    ]
--- kompile/mysite/views.py
+++ kompile/mysite/views.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
 
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the mysite index.")
+    latest_articles = Article.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_articles': latest_articles,
+    }
+    return render(request, 'mysite/index.html', context)
+
+def detail(request, article_id):
+    article = get_object_or_404 (Article, pk=article_id)
+    return render (request, 'mysite/detail.html', {'article': article})
+
+def category_filter(request, category_id):
+    response = "You're looking at the results of category %s."
+    return HttpResponse(response % category_id)
